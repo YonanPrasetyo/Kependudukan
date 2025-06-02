4c8babe65f29e3b8359d6fce59ab4fcc3cc41ae1
YonanPrasetyo <yonanprasetyo91792@gmail.com>
2025-06-02 12:39:42 +0700

    template folder

---
+++ Kependudukan/view/kependudukan/penduduk/add_penduduk.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kependudukan.view.kependudukan.penduduk
+{
+    public partial class add_penduduk: Form
+    {
+        public add_penduduk()
+        {
+            InitializeComponent();
+        }
+    }
+}
